8da75b5fcc1604b5bfbefd8d58203a64268b92e9
Ryan Collins <ryan@ohmgeek.co.uk>
2017-05-25 13:40:19 +0100

    Added basic linkedin stuff, and changed to be a Software Engineer

--- ohmgeek/src/Components/MainPage.js
+++ ohmgeek/src/Components/MainPage.js
@@ -17,18 +17,24 @@ export default class MainPage extends Component {
         <Box>
           <Hero size="medium" backgroundImage="http://forum.us.herozerogame.com/public/style_images/herozero_master/custom/background.jpg" colorIndex="light-1">
             <Heading strong={true} tag="h1">
-              Ryan Collins, Web Developer
+              Ryan Collins, Software Engineer
             </Heading>
           </Hero>
         </Box>
         <Section>
           <Heading strong={false} tag="h2">
-            Find me on the Internet!
+              Connect with me on Social Media
           </Heading>
           <Card heading="GitHub"
                 link={<Anchor href='https://Github.com/OhmGeek'
                               label='View Code' />}
                 thumbnail="https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png"
+                description="Many of my programming projects are on GitHub. Why not take a look?"
+          />
+          <Card heading="LinkedIn"
+                link={<Anchor href='https://www.linkedin.com/in/ryan-j-collins/'
+                              label='Network with me' />}
+                thumbnail="https://content.linkedin.com/content/dam/brand/site/img/policies/member-logo-examples.png"
                 description="Many of my programming projects are on GitHub. Why not take a look?"
           />
         </Section>
